caea7f6b0c5d4a78a67f478159d003e3a81c5794
John Conway <coderconway@gmail.com>
2021-03-08 21:52:47 +0000

    Create phpExercises1.php

---
+++ phpExercises1.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Exercises - 1</title>
+
+
+</head>
+<body style="background-color:bisque;" >
+
+<?php
+
+//These exercises are taken from the website www.phpexercises.com 
+
+//Variables for Exercise 2
+$var1 = "Twinkle";
+$var2 = "star";
+
+//Exercise 1
+echo"<u><b>Here are some examples (including this sentence!) of printing a string to the browser.</u></b><br>";
+echo "This is a php statement.";
+echo "<br>";
+//Exercise2
+echo "$var1, twinkle little $var2";
+echo "<p>";
+//Variables for Exercise 3
+$x = 10;
+$y = 7;
+
+echo "<u><b>Here are some examples of some Arithmetic Operators and Variables.</u></b><br>";
+
+$result = $x+$y;
+echo "$x + $y = $result<br />";
+
+$result = $x-$y;
+echo "$x - $y = $result<br />";
+
+$result = $x*$y;
+echo "$x * $y = $result<br />";
+
+$result = $x/$y;
+echo "$x / $y = $result<br />";
+
+$result = $x%$y;
+echo "$x % $y = $result<br />";
+
+?>
+    
+</body>
+</html>
+
