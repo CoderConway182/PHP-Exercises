e32f33b6f70e22dda12c5393f9eb140e1c477e0c
John Conway <coderconway@gmail.com>
2021-03-10 22:24:55 +0000

    Exercises 2 through 6
    Code from www.phpexercises.com - exercises 2 to 6

---
+++ phpExercises2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Exercises 2</title>
+</head>
+<body  style="background-color:bisque;" >
+    
+    <?php
+
+    $num = "8";
+
+    echo "Value is now $num.<br/>";
+
+    $num +=2;
+
+    echo "Add 2. Value is now $num.<br/>";
+
+    $num -=4;
+    echo "Subtract 4. Value is now $num.<br/>";
+
+    $num *=5;
+    echo "Multiply by 5. Value is now equal to $num.<br/>";
+
+    $num /=3;
+    echo "Divide by 3. Value is now $num.<br/>";
+
+    $num++;
+    echo "Increment by 1. Value is now $num.<br/>";
+
+    $num--;
+    echo "Decrement by 1. Value is now $num.<br/>";
+
+    ?>
+
+</body>
+</html>
---
+++ phpExercises3_4_and_5.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Exercises 3, 4, and 5</title>
+</head>
+<body style="background-color:aliceblue;">
+    
+<?php
+
+    $name='Harry';
+    $age=28;
+
+    var_dump($name);
+    echo "<br/>";
+
+    print_r($name);
+    echo "<br/>";
+
+    var_dump($age);
+    echo "<br/>";
+
+    $name=null;
+
+    var_dump($name);
+
+
+?>
+
+</body>
+</html>
